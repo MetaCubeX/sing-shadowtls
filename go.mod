2f4e90ff0b935592ab530d76aaddbe843aa0f750
wwqgtxx <wwqgtxx@gmail.com>
2025-04-12 20:11:22 +0800

    Remove crypto/tls fork

--- go.mod
+++ go.mod
@@ -2,10 +2,6 @@ module github.com/sagernet/sing-shadowtls
 
 go 1.20
 
-require (
-	github.com/sagernet/sing v0.6.3
-	golang.org/x/crypto v0.32.0
-	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
-)
+require github.com/sagernet/sing v0.6.3
 
-require golang.org/x/sys v0.30.0
+require golang.org/x/sys v0.30.0 // indirect
--- tls_wrapper.go
+++ tls_wrapper.go
@@ -2,10 +2,7 @@ package shadowtls
 
 import (
 	"context"
-	"crypto/tls"
 	"net"
-
-	"github.com/sagernet/sing/common"
 )
 
 type (
@@ -17,28 +14,3 @@ type (
 		sessionIDGenerator TLSSessionIDGeneratorFunc, // for shadow-tls version 3
 	) error
 )
-
-func DefaultTLSHandshakeFunc(password string, config *tls.Config) TLSHandshakeFunc {
-	return func(ctx context.Context, conn net.Conn, sessionIDGenerator TLSSessionIDGeneratorFunc) error {
-		tlsConfig := &sTLSConfig{
-			Rand:                  config.Rand,
-			Time:                  config.Time,
-			VerifyPeerCertificate: config.VerifyPeerCertificate,
-			RootCAs:               config.RootCAs,
-			NextProtos:            config.NextProtos,
-			ServerName:            config.ServerName,
-			InsecureSkipVerify:    config.InsecureSkipVerify,
-			CipherSuites:          config.CipherSuites,
-			MinVersion:            config.MinVersion,
-			MaxVersion:            config.MaxVersion,
-			CurvePreferences: common.Map(config.CurvePreferences, func(it tls.CurveID) sTLSCurveID {
-				return sTLSCurveID(it)
-			}),
-			SessionTicketsDisabled: config.SessionTicketsDisabled,
-			Renegotiation:          sTLSRenegotiationSupport(config.Renegotiation),
-			SessionIDGenerator:     generateSessionID(password),
-		}
-		tlsConn := sTLSClient(conn, tlsConfig)
-		return tlsConn.HandshakeContext(ctx)
-	}
-}
